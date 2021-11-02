78d7b72adcbc21b3e9bc5e10fe957eb90ac966b3
subaleshs <subalesh@qburst.com>
2021-11-02 16:44:38 +0530

    Login app files added

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        val emailField = findViewById<EditText>(R.id.emailField)
+        val passwordField = findViewById<EditText>(R.id.passwordField)
+
+        loginButton.setOnClickListener {
+//            var credntials = getLoginDetails(emailField, passwordField)
+
+
+            if(checkIsEmpty(emailField, passwordField)){
+                var credntials = getLoginDetails(emailField, passwordField)
+            }
+
+        }
+    }
+
+    private fun getLoginDetails(emailField: EditText, passwordField: EditText): Array<String> {
+
+        /*
+        Method to retrieve login credentials entered by the user
+
+        return: unit - Void function
+         */
+
+
+        var email = emailField.text.toString()
+        var password = passwordField.text.toString()
+        println("Email = $email Pass = $password")
+
+        return arrayOf(email, password)
+    }
+
+    private fun checkIsEmpty(emailField: EditText, passwordField: EditText): Boolean {
+
+        var emptyFieldToast: Toast
+
+        if (isEmpty(emailField.text.toString())) {
+            emptyFieldToast = Toast.makeText(baseContext, "Email Field Required.", Toast.LENGTH_LONG)
+            emptyFieldToast.show()
+        }
+        else if (isEmpty(passwordField.text.toString())){
+
+        }
+
+        return true
+    }
+
+}
