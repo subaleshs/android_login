4fb8564d57a5760a2cc731e6240680452b8c5543
subaleshs <subalesh@qburst.com>
2021-11-12 18:07:57 +0530

    Removed unused methods

--- app/src/main/java/com/example/loginapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/loginapp/HomeScreenActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -31,32 +30,13 @@ class HomeScreenActivity : AppCompatActivity() {
                 textField.text = userName.split("@")[0]
             }
 
-            if (loginSharedPreferences.getBoolean("autoLogoutBool", false)){
-                setTimeCounter(loginSharedPreferences)
-            }
 
             val logoutButton = findViewById<Button>(R.id.logoutButton)
-
             logoutButton.setOnClickListener {
                 showLogoutConfirm(loginSharedPreferences)
             }
         }
 
-    }
-
-    private fun setTimeCounter(sharedPreferences: SharedPreferences){
-        val timer = object : CountDownTimer(10000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                // do nothing
-                val timerText = findViewById<TextView>(R.id.timer)
-                timerText.text = "${millisUntilFinished/1000}"
-            }
-
-            override fun onFinish() {
-                showLogoutConfirm(sharedPreferences)
-            }
-        }
-        timer.start()
     }
 
     private fun logOut(sharedPreferences: SharedPreferences) {
@@ -70,32 +50,18 @@ class HomeScreenActivity : AppCompatActivity() {
     private fun showLogoutConfirm(sharedDetails: SharedPreferences){
 
         val logoutConfirmAlert = AlertDialog.Builder(this)
-
         logoutConfirmAlert.setMessage("Press Confirm to Logout")
-
-        logoutConfirmAlert.setPositiveButton("Confirm"){
-                _, _ -> logOut(sharedDetails)
-        }
-        logoutConfirmAlert.setNegativeButton("Cancel"){
-                dialog, _ -> dialog.dismiss()
-        }
+        logoutConfirmAlert.setPositiveButton("Confirm"){ _, _ -> logOut(sharedDetails) }
+        logoutConfirmAlert.setNegativeButton("Cancel"){ dialog, _ -> dialog.dismiss() }
         logoutConfirmAlert.show()
     }
 
     override fun onBackPressed() {
 
         val exitConfirm = AlertDialog.Builder(this)
-
         exitConfirm.setMessage("Do you want to exit?")
-
-        exitConfirm.setPositiveButton("Confirm"){
-                _, _ -> finishAffinity()
-        }
-
-        exitConfirm.setNegativeButton("Cancel"){
-                dialog, _ -> dialog.cancel()
-        }
-
+        exitConfirm.setPositiveButton("Confirm"){ _, _ -> finishAffinity() }
+        exitConfirm.setNegativeButton("Cancel"){ dialog, _ -> dialog.cancel() }
         exitConfirm.show()
     }
 
