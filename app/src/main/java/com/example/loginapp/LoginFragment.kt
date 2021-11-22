183866930b53a1afd5c0626da3c6661e7c7caa2a
subaleshs <subalesh@qburst.com>
2021-11-22 17:58:14 +0530

    Login functionality added

--- app/src/main/java/com/example/loginapp/LoginFragment.kt
+++ app/src/main/java/com/example/loginapp/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.loginapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
@@ -9,11 +10,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : Fragment() {
 
@@ -31,8 +35,30 @@ class LoginFragment : Fragment() {
 
         val forgotPasswordTextView = view.findViewById<TextView>(R.id.forgotPasswordTextView)
         val signUpText =  view.findViewById<TextView>(R.id.signUpTextView)
+        val loginButton = view.findViewById<Button>(R.id.loginButtonView)
+        val emailField = view.findViewById<TextInputEditText>(R.id.emailTextView)
+        val passwordField = view.findViewById<TextInputEditText>(R.id.passwordTextView)
+        val emailTextInputLayout = view.findViewById<TextInputLayout>(R.id.emailTextInputLayout)
+        val passwordTextInputLayout = view.findViewById<TextInputLayout>(R.id.passwordTextInputLayout)
+
+        emailField.addTextChangedListener { emailTextInputLayout.error = null }
+        passwordField.addTextChangedListener { passwordTextInputLayout.error = null }
 
         showSignUpText(signUpText)
+
+        loginButton.setOnClickListener {
+
+            val userEmailAddress = emailField.text.toString()
+            val userPassword = passwordField.text.toString()
+
+            val loginClass = Login(userEmailAddress, userPassword)
+
+            if (loginClass.checkLoginField(emailTextInputLayout, passwordTextInputLayout)) {
+                if (loginClass.authenticateUser(emailTextInputLayout, passwordTextInputLayout)) {
+                    changeToHomeScreen( userEmailAddress, userPassword)
+                }
+            }
+        }
 
         signUpText.setOnClickListener {
             val signUpFragmentTransaction = parentFragmentManager.beginTransaction()
@@ -56,6 +82,24 @@ class LoginFragment : Fragment() {
             }
 
         }
+    }
+
+    private fun changeToHomeScreen (emailAddress:String, password: String) {
+        /*
+        Method to switch to home screen
+         */
+
+        val loginSharedPreferences = activity?.getSharedPreferences("user",
+            AppCompatActivity.MODE_PRIVATE
+        )
+        val loginEditor = loginSharedPreferences?.edit()
+        loginEditor?.putString("email", emailAddress)
+        loginEditor?.putString("password", password)
+        loginEditor?.apply()
+
+        val activityIntent = Intent(activity, HomeScreenActivity::class.java)
+        startActivity(activityIntent)
+        activity?.finish()
     }
 
     
--- app/src/main/java/com/example/loginapp/LoginScreenActivity.kt
+++ app/src/main/java/com/example/loginapp/LoginScreenActivity.kt
@@ -5,70 +5,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginScreenActivity : AppCompatActivity() {
+class LoginScreenActivity : AppCompatActivity(){
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val appContext = applicationContext
         val loginDetailsPreferences = getSharedPreferences("user", MODE_PRIVATE)
-
         if (loginDetailsPreferences.contains("email") && loginDetailsPreferences.contains("password")){
 
-            val activityIntent = Intent(appContext, HomeScreenActivity::class.java)
+            val activityIntent = Intent(this, HomeScreenActivity::class.java)
             startActivity(activityIntent)
         }
         else{
 
             setContentView(R.layout.activity_login_screen)
 
-//            val loginButton = findViewById<Button>(R.id.loginButtonView)
-//            val emailField = findViewById<EditText>(R.id.emailTextView)
-//            val passwordField = findViewById<EditText>(R.id.passwordTextView)
-//            val emailTextInputLayout = findViewById<TextInputLayout>(R.id.emailTextInputLayout)
-//            val passwordTextInputLayout = findViewById<TextInputLayout>(R.id.passwordTextInputLayout)
-//
-//            val signupTextField = findViewById<TextView>(R.id.signUpTextView)
-//            showSignUpText(signupTextField)
-//            signupTextField.setOnClickListener {
-//                println("signUp")
-//                Toast.makeText(this, "SIGNUP", Toast.LENGTH_SHORT).show()
-//            }
-//
-//
-//            emailField.addTextChangedListener { emailTextInputLayout.error = null }
-//            passwordField.addTextChangedListener { passwordTextInputLayout.error = null }
-
-//            loginButton.setOnClickListener {
-//                val userEmailAddress = emailField.text.toString()
-//                val userPassword = passwordField.text.toString()
-//
-//                val loginClass = Login(userEmailAddress, userPassword)
-//
-//                if (loginClass.checkLoginField(emailTextInputLayout, passwordTextInputLayout)) {
-//                    if (loginClass.authenticateUser(emailTextInputLayout, passwordTextInputLayout)){
-//                        changeToHomeScreen(appContext, userEmailAddress, userPassword)
-//                    }
-//                }
-//            }
-
         }
 
     }
 
-    private fun changeToHomeScreen (activityContext: Context, emailAddress:String, password: String) {
-        /*
-        Method to switch to home screen
-         */
+    override fun onBackPressed() {
+        val backStackCount = supportFragmentManager.backStackEntryCount
 
-        val loginSharedPreferences = getSharedPreferences("user", MODE_PRIVATE)
-        val loginEditor = loginSharedPreferences.edit()
-        loginEditor.putString("email", emailAddress)
-        loginEditor.putString("password", password)
-        loginEditor.apply()
-
-        val activityIntent = Intent(activityContext, HomeScreenActivity::class.java)
-        startActivity(activityIntent)
+        if (backStackCount == 0){
+            moveTaskToBack(true)
+        }
+        else{
+            super.onBackPressed()
+        }
     }
+
+
 }
 
