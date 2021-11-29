e7c51cf98f7921bdee91931bb7dae67e1b9cfc92
subaleshs <subalesh@qburst.com>
2021-11-29 11:42:56 +0530

    Added logout button to account fragment

--- app/src/main/java/com/example/loginapp/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/loginapp/fragments/AccountFragment.kt
@@ -1,15 +1,23 @@
 package com.example.loginapp.fragments
 
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginapp.HomeScreenActivity
+import com.example.loginapp.LoginScreenActivity
 import com.example.loginapp.R
 import com.example.loginapp.databinding.FragmentAccountBinding
 
 
-class AccountFragment : Fragment() {
+class AccountFragment(private val sharedPreferences: SharedPreferences, private val activity: Context) : Fragment() {
 
     private lateinit var accountFragmentBinding: FragmentAccountBinding
 
@@ -26,6 +34,26 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        accountFragmentBinding.loginButtonView.setOnClickListener { showLogoutDialog()}
+
+    }
+
+    private fun showLogoutDialog(){
+
+        val logoutConfirmAlert = AlertDialog.Builder(activity)
+        logoutConfirmAlert.setMessage("Press Confirm to Logout")
+        logoutConfirmAlert.setPositiveButton("Confirm"){ _, _ -> logOut() }
+        logoutConfirmAlert.setNegativeButton("Cancel"){ dialog, _ -> dialog.dismiss() }
+        logoutConfirmAlert.show()
+    }
+
+    private fun logOut() {
+        val loginDetailEditor = sharedPreferences.edit()
+        loginDetailEditor.clear()
+        loginDetailEditor.apply()
+
+        val activityIntent = Intent(activity, LoginScreenActivity::class.java)
+        startActivity(activityIntent)
     }
 
 }
