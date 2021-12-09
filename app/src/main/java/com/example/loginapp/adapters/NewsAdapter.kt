5df6f51b867cbcc5c13b30907032b40d1d8ffe53
subaleshs <subalesh@qburst.com>
2021-12-09 15:18:49 +0530

    Add loading image from URL

--- app/src/main/java/com/example/loginapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/loginapp/adapters/NewsAdapter.kt
@@ -5,35 +5,47 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.loginapp.NewsData
 import com.example.loginapp.NewsContent
+import com.example.loginapp.R
 import com.example.loginapp.databinding.NewsRecylcerLayoutBinding
 
-class NewsAdapter(private val newsData: NewsData): RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter(private val newsData: NewsData) :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+    var onCardClick: ((NewsContent) -> Unit)? = null
 
     lateinit var context: Context
-    var onCardClick: ((NewsContent)-> Unit)? = null
 
-    class NewsViewHolder(val binding: NewsRecylcerLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+    class NewsViewHolder(val binding: NewsRecylcerLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(newsData: NewsContent, category: String){
-            val (title, date) = newsData
+        fun bind(newsData: NewsContent, category: String, context: Context) {
+            val (title, date, content, url) = newsData
 
             binding.category.text = category
             binding.newsTitle.text = title
             binding.dateView.text = date
+            Log.d("ur", url)
+            Glide.with(context).load(url)
+                .error(R.drawable.news)
+                .into(binding.newsPicture)
+
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
 
-        val viewBinding = NewsRecylcerLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewBinding =
+            NewsRecylcerLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        context = parent.context
         return NewsViewHolder(viewBinding)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        Log.d("pos",position.toString())
-        holder.bind(newsData.data[position], newsData.category)
+        Log.d("pos", position.toString())
+        holder.bind(newsData.data[position], newsData.category, context)
         holder.binding.newsCard.setOnClickListener {
 
             onCardClick?.invoke(newsData.data[position])
--- app/src/main/java/com/example/loginapp/fragments/NewsFeedFragment.kt
+++ app/src/main/java/com/example/loginapp/fragments/NewsFeedFragment.kt
@@ -1,12 +1,10 @@
 package com.example.loginapp.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
 import com.example.loginapp.NewsData
 import com.example.loginapp.NewsContent
 import com.example.loginapp.R
@@ -19,7 +17,7 @@ import java.io.IOException
 class NewsFeedFragment : Fragment() {
 
 
-    var onNewsExpand: ((NewsContent)-> Unit)? = null
+    var onNewsExpand: ((NewsContent) -> Unit)? = null
     private lateinit var newsFragmentBinding: FragmentNewsFeedBinding
 
     override fun onCreateView(
@@ -41,7 +39,7 @@ class NewsFeedFragment : Fragment() {
         val newsAdapter = NewsAdapter(news)
 
         newsFragmentBinding.newsRecyclerLayout.adapter = newsAdapter
-        newsAdapter.onCardClick = {detailedNews: NewsContent ->
+        newsAdapter.onCardClick = { detailedNews: NewsContent ->
 
             val transaction = parentFragmentManager.beginTransaction()
             val fragment = DetailedNewsFragment()
@@ -65,22 +63,19 @@ class NewsFeedFragment : Fragment() {
     }
 
 
-
-    private fun getNewsData(json: String): NewsData{
-
-            return Gson().fromJson(json, NewsData::class.java)
+    private fun getNewsData(json: String?): NewsData {
+        return Gson().fromJson(json, NewsData::class.java)
     }
 
 
-    private fun getJSONString(): String {
+    private fun getJSONString(): String? {
 
-        var json = "{}"
+        var json: String? = null
 
         try {
             val jsonFile = context?.assets?.open("news.json")
-            json =  jsonFile?.bufferedReader().use { it!!.readText() }
-        }
-        catch (exception: IOException){
+            json = jsonFile?.bufferedReader()?.use { it.readText() }
+        } catch (exception: IOException) {
             exception.printStackTrace()
             return json
         }
