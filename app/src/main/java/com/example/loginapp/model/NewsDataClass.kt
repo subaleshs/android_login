b752a11bb24de4ed7be65e7846e2d2888e3c4ca1
subaleshs <subalesh@qburst.com>
2021-12-17 18:47:24 +0530

    Make the variables nullable

--- app/src/main/java/com/example/loginapp/model/NewsDataClass.kt
+++ app/src/main/java/com/example/loginapp/model/NewsDataClass.kt
@@ -2,15 +2,15 @@ package com.example.loginapp.model
 
 
 data class NewsContent(
-    val title: String,
-    val date: String,
-    val content: String,
-    val imageUrl: String,
-    val readMoreUrl: String
+    val title: String?,
+    val date: String?,
+    val content: String?,
+    val imageUrl: String?,
+    val readMoreUrl: String?
 )
 
 data class NewsData(
-    val category: String,
-    val data: ArrayList<NewsContent>
+    val category: String?,
+    val data: ArrayList<NewsContent>?
 )
 
