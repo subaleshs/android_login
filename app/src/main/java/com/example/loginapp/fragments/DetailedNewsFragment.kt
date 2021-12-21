bb44d76aab6a5634105985578df48967429c547f
subaleshs <subalesh@qburst.com>
2021-12-21 09:39:34 +0530

    Add style for network error text

--- app/src/main/java/com/example/loginapp/fragments/DetailedNewsFragment.kt
+++ app/src/main/java/com/example/loginapp/fragments/DetailedNewsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import com.bumptech.glide.Glide
 import com.example.loginapp.R
@@ -21,6 +22,7 @@ class DetailedNewsFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentDetailedNewsBinding.inflate(inflater, container, false)
         val newsDetail = arguments?.getStringArrayList("news")
+
         binding.newsTitleText.text = newsDetail?.get(0) ?: "title"
         binding.newsContent.text = newsDetail?.get(1) ?: "Content"
         context?.let { Glide.with(it).load(newsDetail?.get(4)).error(R.drawable.news).into(binding.newsImage) }
