58aa13975ce8defc62cd45d0e14a7bdc8ec871a1
subaleshs <subalesh@qburst.com>
2021-12-30 16:58:24 +0530

    Add camera access

--- app/src/main/java/com/example/loginapp/activities/LoginScreenActivity.kt
+++ app/src/main/java/com/example/loginapp/activities/LoginScreenActivity.kt
@@ -1,11 +1,14 @@
 package com.example.loginapp.activities
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.example.loginapp.databinding.ActivityLoginScreenBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.util.jar.Manifest
 
 class LoginScreenActivity : AppCompatActivity() {
 
@@ -15,6 +18,10 @@ class LoginScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 111)
+        }
         if (FirebaseAuth.getInstance().currentUser != null) {
 
             val activityIntent = Intent(this, HomeScreenActivity::class.java)
--- app/src/main/java/com/example/loginapp/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/loginapp/fragments/AccountFragment.kt
@@ -1,19 +1,30 @@
 package com.example.loginapp.fragments
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.loginapp.activities.LoginScreenActivity
 import com.example.loginapp.R
 import com.example.loginapp.databinding.FragmentAccountBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.lang.NullPointerException
 
 
 class AccountFragment() : Fragment() {
@@ -39,7 +50,58 @@ class AccountFragment() : Fragment() {
         accountFragmentBinding.userName.text = email?.split('@')?.get(0) ?: "No Username"
         accountFragmentBinding.loginButtonView.setOnClickListener { showLogoutDialog() }
 
+        val result = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                val imgBitmap = result.data?.extras?.get("data") as Bitmap
+                accountFragmentBinding.profileImage.setImageBitmap(imgBitmap)
+            }else{
+                Log.d("failcam", "fail")
+            }
+        }
+        val takePic = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+
+        accountFragmentBinding.profileImage.setOnClickListener {
+            if (checkCamPermission()) {
+                result.launch(takePic)
+            }
+            else{
+                ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.CAMERA), 111)
+            }
+        }
+
     }
+
+    private fun performCrop(imageUri: Uri?, p: ActivityResultLauncher<Intent>) {
+        try {
+            val cropIntent = Intent("com.android.camera.action.CROP")
+            cropIntent.setDataAndType(imageUri, "image/*")
+            cropIntent.putExtra("crop", "true")
+            // indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 2)
+            cropIntent.putExtra("aspectY", 1)
+            // indicate output X and Y
+            cropIntent.putExtra("outputX", 256)
+            cropIntent.putExtra("outputY", 256)
+            // retrieve data on return
+            cropIntent.putExtra("return-data", true)
+            p.launch(cropIntent)
+
+        }catch (e: ActivityNotFoundException) {
+           Log.d("crop","This device doesn't support the crop action!")
+        }
+
+
+    }
+
+    private fun checkCamPermission(): Boolean {
+        return try {
+            ActivityCompat.checkSelfPermission(activity!!, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        }catch (exception: NullPointerException){
+            false
+        }
+    }
+
 
     override fun onResume() {
         super.onResume()
