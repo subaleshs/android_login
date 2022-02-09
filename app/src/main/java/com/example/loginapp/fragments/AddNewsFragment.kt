da76805d0f6f5cac8a9ead57a9bb25c54767d6c9
subaleshs <subalesh@qburst.com>
2022-02-09 14:33:18 +0530

    Storing captured image in API 29 and above added

--- app/src/main/java/com/example/loginapp/fragments/AddNewsFragment.kt
+++ app/src/main/java/com/example/loginapp/fragments/AddNewsFragment.kt
@@ -2,8 +2,10 @@ package com.example.loginapp.fragments
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
@@ -24,9 +26,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.loginapp.R
 import com.example.loginapp.databinding.FragmentAddNewsBinding
+import com.example.loginapp.utils.APILevelCheck
 import com.example.loginapp.viewmodel.CustomNewsViewModel
+import java.io.BufferedInputStream
 import java.io.File
-import java.io.IOException
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class AddNewsFragment : Fragment() {
 
@@ -37,6 +42,7 @@ class AddNewsFragment : Fragment() {
     private var writePermission: Boolean = false
     private val requestPermission: MutableList<String> = mutableListOf()
     private var imageFile: File? = null
+    private var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,26 +56,37 @@ class AddNewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(CustomNewsViewModel::class.java)
 
-        val result = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                if (imageFile != null) {
-                    val bmp = BitmapFactory.decodeFile(imageFile?.absolutePath)
-                    Log.d("imgF", imageFile?.absolutePath.toString())
-                    binding.capImage.setImageBitmap(bmp)
-                    context?.sendBroadcast(
-                        Intent(
-                            Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                            Uri.parse("file://" + imageFile?.absolutePath)
+        val result =
+            this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    if (imageFile != null) {
+                        val bmp = BitmapFactory.decodeFile(imageFile?.absolutePath)
+                        Log.d("imgF", imageFile?.absolutePath.toString())
+                        binding.capImage.setImageBitmap(bmp)
+                        context?.sendBroadcast(
+                            Intent(
+                                Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                                Uri.parse("file://" + imageFile?.absolutePath)
+                            )
                         )
-                    )
+                    } else {
+                        if (imageUri != null) {
+                            var fileInputStream: InputStream? = null
+                            try {
+                                fileInputStream =
+                                    context?.contentResolver?.openInputStream(imageUri!!)
+                            } catch (exception: FileNotFoundException) {
+                                showExceptionAlert(R.string.file_creation_error)
+                            }
+                            val image = fileInputStream?.let { BitmapFactory.decodeStream(it) }
+                            binding.capImage.setImageBitmap(image)
+
+                        }
+                    }
+
                 }
-
             }
-        }
         val takePic = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-//        takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-
 
         binding.newNewsTitle.addTextChangedListener {
             binding.titleTextInputLayout.error = ""
@@ -84,39 +101,84 @@ class AddNewsFragment : Fragment() {
         binding.appCompatImageButton.setOnClickListener {
             if (checkCameraPermission()) {
                 checkReadwritePermission()
-                if (requestPermission.size <=0) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.d("per", "$readPermission $writePermission $requestPermission")
+                if (requestPermission.size <= 0) {
+                    if (APILevelCheck.isAPIGreaterThan28()) {
+                        val dirPath = Environment.DIRECTORY_PICTURES + "/Truth"
+                        context?.applicationContext?.getExternalFilesDir(dirPath)
+                        val time = System.currentTimeMillis().toString()
+                        val imageName = "IMG_$time.jpg"
+                        val contentValues = ContentValues().apply {
+                            put(MediaStore.MediaColumns.DISPLAY_NAME, imageName)
+                            put(MediaStore.MediaColumns.MIME_TYPE, "image/*")
+                            put(MediaStore.MediaColumns.RELATIVE_PATH, dirPath)
+                        }
 
-                    } else {
-                        createTemImageFile()
-                        if (imageFile != null) {
-                            val uri = FileProvider.getUriForFile(requireContext(), "com.example.loginapp.provider",
-                                imageFile!!
+                        val resolver = context?.contentResolver
+                        if (resolver != null) {
+                            imageUri = resolver.insert(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                contentValues
                             )
-                            val resolveInfoList = context?.packageManager?.queryIntentActivities(takePic, PackageManager.MATCH_DEFAULT_ONLY)
-                            Log.d("resolve", resolveInfoList.toString())
-                            if (resolveInfoList != null) {
-                                for (resInfo in resolveInfoList) {
-                                    val packageName = resInfo.activityInfo.packageName
-                                    context?.grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                                }
+                            if (imageUri != null) {
+                                takePic.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                                result.launch(takePic)
                             }
-                            takePic.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-                            result.launch(takePic)
+
+                        }
+                    } else {
+                        if (false) {
+                            val selectPic = Intent(Intent.ACTION_GET_CONTENT)
+                            selectPic.type = "image/*"
+                            result.launch(selectPic)
+                        } else {
+                            createTemImageFile()
+                            if (imageFile != null) {
+                                val uri = FileProvider.getUriForFile(
+                                    requireContext(), "com.example.loginapp.provider",
+                                    imageFile!!
+                                )
+                                val resolveInfoList =
+                                    context?.packageManager?.queryIntentActivities(
+                                        takePic,
+                                        PackageManager.MATCH_DEFAULT_ONLY
+                                    )
+                                Log.d("resolve", resolveInfoList.toString())
+                                if (resolveInfoList != null) {
+                                    for (resInfo in resolveInfoList) {
+                                        val packageName = resInfo.activityInfo.packageName
+                                        context?.grantUriPermission(
+                                            packageName,
+                                            uri,
+                                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                                        )
+                                    }
+                                }
+                                takePic.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+                                result.launch(takePic)
+                            }
                         }
                     }
                 } else {
-                    ActivityCompat.requestPermissions(requireContext() as Activity, requestPermission.toTypedArray(), 111)
+                    ActivityCompat.requestPermissions(
+                        requireContext() as Activity,
+                        requestPermission.toTypedArray(),
+                        111
+                    )
                 }
             } else {
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA), 101)
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    101
+                )
             }
         }
         binding.addNewsButton.setOnClickListener {
             val title: String = binding.newNewsTitle.text.toString()
             val date: String = binding.date.text.toString()
             val content: String = binding.content.text.toString()
-            if (checkForEmptyFields(title, date, content)){
+            if (checkForEmptyFields(title, date, content)) {
 //                var news = CustomNewsEntity(
 //                    title,
 //                    content,
@@ -129,34 +191,36 @@ class AddNewsFragment : Fragment() {
         }
     }
 
-    private fun createTemImageFile(){
-        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val storageDirectory = File(path,"/Truth")
-        if (!storageDirectory.exists()){
+    private fun createTemImageFile() {
+        val path: File? = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val storageDirectory = File(path, "/Truth")
+        if (!storageDirectory.exists()) {
             if (!storageDirectory.mkdir()) {
                 Toast.makeText(requireContext(), "cant create dir", Toast.LENGTH_SHORT).show()
             }
         }
-        Log.d("abs", storageDirectory.absolutePath)
         val time = System.currentTimeMillis().toString()
         val imageName = "IMG_$time"
-        Log.d("dir", "$storageDirectory $imageName")
         try {
             imageFile = File.createTempFile(imageName, ".jpg", storageDirectory).apply {
                 createNewFile()
             }
         } catch (exception: Exception) {
-            AlertDialog.Builder(requireContext()).setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle(R.string.error)
-                .setMessage(R.string.file_creation_error)
+            showExceptionAlert(R.string.file_creation_error)
         }
     }
 
     private fun checkReadwritePermission() {
-        readPermission = ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-        writePermission = ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+        readPermission = ActivityCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+        writePermission = ActivityCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
         requestPermission.clear()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !readPermission) {
+        if (APILevelCheck.isAPIGreaterThan28() && !readPermission) {
             requestPermission.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         } else {
             if (!readPermission) {
@@ -169,7 +233,10 @@ class AddNewsFragment : Fragment() {
     }
 
     private fun checkCameraPermission(): Boolean {
-        return ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun checkForEmptyFields(title: String, date: String, content: String): Boolean {
@@ -182,12 +249,22 @@ class AddNewsFragment : Fragment() {
             notEmpty = false
             binding.dateTextInputLayout.error = getString(R.string.input_error)
         }
-        if (TextUtils.isEmpty(content)){
+        if (TextUtils.isEmpty(content)) {
             notEmpty = false
             binding.contentTextInputLayout.error = getString(R.string.input_error)
         }
 
         return notEmpty
+    }
+
+    private fun showExceptionAlert(errorStringResource: Int) {
+        AlertDialog.Builder(requireContext()).setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle(R.string.error)
+            .setMessage(errorStringResource)
+            .setPositiveButton(R.string.ok) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
 
