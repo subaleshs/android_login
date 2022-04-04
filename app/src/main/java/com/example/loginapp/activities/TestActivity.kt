2aad58e57ddaae0d0d1d761cf810b5306ae5ae91
subaleshs <subalesh@qburst.com>
2022-04-04 14:20:27 +0530

    test push

---
+++ app/src/main/java/com/example/loginapp/activities/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
